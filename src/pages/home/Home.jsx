78211bf25259fbd810c05a07254d7140c1ac81c7
FernandoMacacao <80410580+FernandoMacacao@users.noreply.github.com>
2023-04-07 23:08:30 +0100

    More things done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,21 @@ import { Banner } from "./components/Banner";
 import { Info } from "./components/Info";
 import { Accommodations } from "./components/Accommodations";
 
+const data = [
+  {
+    title: "Alojamentos Locais",
+    text: "Donec vitae pulvinar leo. Sed interdum laoreet ornare. Phasellus convallis euismod volutpat. In lacus felis",
+    image: "acc_2.jpg",
+    link: "/",
+  },
+  {
+    title: "Alojamentos de média e longa duração",
+    text: "Donec vitae pulvinar leo. Sed interdum laoreet ornare. Phasellus convallis euismod volutpat. In lacus felis",
+    image: "acc_1.jpg",
+    link: "/",
+  },
+];
+
 export const Home = () => {
   return (
     <div>
@@ -18,7 +33,7 @@ export const Home = () => {
         title="Lorem ipsum dolor sit amet"
         text="Pellentesque orci eros, accumsan vitae facilisis ut, pharetra vitae enim. Aliquam tempus blandit placerat. Nam suscipit fermentum neque, non congue ligula iaculis in. Ut tempor auctor arcu. Sed mauris dolor, ullamcorper quis vulputate eu, gravida quis eros. Praesent feugiat ornare dui non consequat."
       />
-      <Accommodations />
+      <Accommodations data={data} />
     </div>
   );
 };
--- src/pages/home/components/Accommodations.jsx
+++ src/pages/home/components/Accommodations.jsx
@@ -1,6 +1,77 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  Container,
+  Typography,
+  Button,
+  useMediaQuery,
+} from "@mui/material";
 import React from "react";
+import { LinkWithScroll } from "common/components/LinkWithScroll";
 
-export const Accommodations = () => {
-  return <Box my={10}>ola</Box>;
+export const Accommodations = ({ data }) => {
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("md"));
+
+  return (
+    <Box
+      my={10}
+      width="100vw"
+      display="flex"
+      flexDirection={{ xs: "column", md: "row" }}
+      alignItems="center"
+      justifyContent="center"
+    >
+      {data.map((d, id) => (
+        <Box
+          key={id}
+          sx={{
+            backgroundImage: `url(${require(`assets/images/${d.image}`)})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            maxWidth: "100vw",
+            minHeight: "50vh",
+          }}
+        >
+          <Box
+            minHeight="50vh"
+            maxWidth="100vw"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Container
+              maxWidth="xl"
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Typography variant="h6" color="secondary" textAlign="center">
+                {d.title}
+              </Typography>
+              <Typography
+                variant="body1"
+                color="secondary"
+                my={4}
+                textAlign="center"
+              >
+                {d.text}
+              </Typography>
+              <LinkWithScroll to={d.link} style={{ textDecoration: "none" }}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth={isMobile}
+                >
+                  Saiba Mais!
+                </Button>
+              </LinkWithScroll>
+            </Container>
+          </Box>
+        </Box>
+      ))}
+    </Box>
+  );
 };
