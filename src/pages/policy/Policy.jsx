7373acfa4b27d778b1ef2a02b16f3e9d989e510d
FernandoMacacao <80410580+FernandoMacacao@users.noreply.github.com>
2023-04-08 20:18:06 +0100

    Policy page implemented

--- src/pages/policy/Policy.jsx
+++ src/pages/policy/Policy.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import { SectionHeader } from "common/components/SectionHeader";
+import React, { useContext } from "react";
+import Typography from "@mui/material/Typography";
+import { LanguageContext } from "configuration/app-context-manager/LanguageContext";
+import Data from "./data/index.json";
+import { Box, Container } from "@mui/material";
 
 export const Policy = () => {
-  return <div></div>;
+  const { language } = useContext(LanguageContext);
+  return (
+    <Box my={10}>
+      <SectionHeader
+        title={Data[language].title}
+        subtitle={Data[language].subtitle}
+      />
+      <Container maxWidth="md">
+        {Data[language].texts.map((t, id) => (
+          <Typography
+            variant="body1"
+            color="textPrimary"
+            key={id}
+            mb={id === 3 ? undefined : 2}
+            textAlign="center"
+          >
+            {t.text}
+          </Typography>
+        ))}
+      </Container>
+    </Box>
+  );
 };
