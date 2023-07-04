0b66a1da41f5f603d824b15edac1f86bd4ae9664
RicardoRocha27 <80410580+FernandoMacacao@users.noreply.github.com>
2023-07-04 19:42:39 +0100

    Accommodation images update (home)

--- src/pages/home/components/Accommodations.jsx
+++ src/pages/home/components/Accommodations.jsx
@@ -1,79 +1,17 @@
-import {
-  Box,
-  Container,
-  Typography,
-  Button,
-  useMediaQuery,
-} from "@mui/material";
-import { LanguageContext } from "configuration/app-context-manager/LanguageContext";
-import React, { useContext } from "react";
-import { LinkWithScroll } from "common/components/LinkWithScroll";
+import { Box } from "@mui/material";
+import React from "react";
+import AccommodationImage from "./AccommodationImage";
 
 export const Accommodations = ({ data }) => {
-  const { language } = useContext(LanguageContext);
-  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("md"));
-
   return (
     <Box
       my={10}
       width="100vw"
       display="flex"
       flexDirection={{ xs: "column", md: "row" }}
-      alignItems="center"
-      justifyContent="center"
     >
-      {data.map((d, id) => (
-        <Box
-          key={id}
-          sx={{
-            backgroundImage: `url(${require(`assets/images/${d.image}`)})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            maxWidth: "100vw",
-            minHeight: "50vh",
-          }}
-        >
-          <Box
-            minHeight="50vh"
-            maxWidth="100vw"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-          >
-            <Container
-              maxWidth="xl"
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <Typography variant="h6" color="secondary" textAlign="center">
-                {d.title}
-              </Typography>
-              <Typography
-                variant="body1"
-                color="secondary"
-                my={4}
-                textAlign="center"
-              >
-                {d.text}
-              </Typography>
-              <LinkWithScroll to={d.link} style={{ textDecoration: "none" }}>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  fullWidth={isMobile}
-                >
-                  {language === "pt" ? "Saiba Mais" : "See More"}
-                </Button>
-              </LinkWithScroll>
-            </Container>
-          </Box>
-        </Box>
-      ))}
+      <AccommodationImage data={data[0]} />
+      <AccommodationImage data={data[1]} />
     </Box>
   );
 };
