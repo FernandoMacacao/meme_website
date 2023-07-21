8f53d3056a433f3ab2776c71af89bf492af7c64c
Tiago Pereira <tiagorp2004@gmail.com>
2023-07-21 13:33:36 +0100

    Texto a dar render automático, falta as fotos

--- src/pages/accommodations/[accommodationId]/components/InfoSection.jsx
+++ src/pages/accommodations/[accommodationId]/components/InfoSection.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Grid, Stack, Typography } from "@mui/material";
 import { LanguageContext } from "configuration/app-context-manager/LanguageContext";
 import { useLocation } from "react-router";
+import { LinkWithScroll } from "common/components/LinkWithScroll";
 
 const InfoSection = ({ data }) => {
   const { language } = useContext(LanguageContext);
@@ -21,6 +22,7 @@ const InfoSection = ({ data }) => {
 
     return accommodationInfo;
   }
+  const specificAcc = getAccommodationInfo();
 
   return (
     <>
@@ -30,51 +32,48 @@ const InfoSection = ({ data }) => {
             <Typography variant="h6" color={"var(--clr-primary)"}>
               {getAccommodationInfo().name}
             </Typography>
-            <Typography>Número de camas:</Typography>
-            <Typography>Número de casas de banho:</Typography>
-            <Typography>Número de hospedes:</Typography>
+            <Typography>{specificAcc.personNumber}</Typography>
+          </Stack>
+          <Stack mb={5}>
+            <Typography variant="h6" color={"var(--clr-primary)"}>
+              {language === "pt" ? "Descrição" : "Description"}
+            </Typography>
+            <Typography>{specificAcc.description}</Typography>
           </Stack>
           <Stack>
             <Typography variant="h6" color={"var(--clr-primary)"}>
-              DESCRIÇÃO
+              {language === "pt" ? "Alguma Dúvida?" : "Want to know more?"}
             </Typography>
-            <Typography>
-              Commodo ex veniam cupidatat dolor aliqua. Quis veniam esse eiusmod
-              fugiat pariatur aliquip ipsum in magna occaecat anim est laboris.
-              Nulla ipsum enim sit non ad minim est consectetur amet duis qui
-              cupidatat ipsum. Esse aute culpa sint irure voluptate magna do
-              consequat eu ea voluptate amet pariatur ea.
-            </Typography>
+            <LinkWithScroll
+              style={{
+                color: "black",
+              }}
+              to={`/${language}/contacts`}
+            >
+              <Typography>
+                {language === "pt" ? "Contacte-nos" : "Contact us"}
+              </Typography>
+            </LinkWithScroll>
           </Stack>
         </Grid>
         <Grid item xs={12} md={6} mb={10}>
           <Typography variant="h6" mb={3} color={"var(--clr-primary)"}>
-            COMODIDADES
+            {language === "pt" ? "Comodidades" : "Commodities"}
           </Typography>
           <Grid container spacing={3} mb={5}>
-            <Grid item xs={6}>
-              <Typography>Cozinha:</Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography>Casa de Banho:</Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography>Quartos:</Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography>Sala:</Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography>Dispensa:</Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography>Escritório:</Typography>
-            </Grid>
+            {Object.keys(specificAcc.comodities).map((key, index) => {
+              return (
+                <Grid item xs={6}>
+                  <Typography fontWeight={600} fontSize={16} key={index}>
+                    {key}:
+                  </Typography>
+                  {specificAcc.comodities[key].map((value, subIndex) => {
+                    return <Typography key={subIndex}>{value}</Typography>;
+                  })}
+                </Grid>
+              );
+            })}
           </Grid>
-          <Typography variant="h6" color={"var(--clr-primary)"}>
-            ALGUMA DUVIDA?
-          </Typography>
-          <Typography>Contacte-nos</Typography>
         </Grid>
       </Grid>
     </>
