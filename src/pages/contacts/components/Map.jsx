d4cb582fe5c91501c860ff3346ab14d6197ab2cb
RicardoRocha27 <80410580+FernandoMacacao@users.noreply.github.com>
2023-07-24 13:20:59 +0100

    unused imports removed

--- src/pages/contacts/components/Map.jsx
+++ src/pages/contacts/components/Map.jsx
@@ -1,16 +1,7 @@
-import { Box, Container, Typography } from "@mui/material";
-import { useLoadScript, GoogleMap, MarkerF } from "@react-google-maps/api";
-import React, { useMemo } from "react";
+import { Box, Container } from "@mui/material";
+import React from "react";
 
 export const Map = () => {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-  });
-
-  const center = useMemo(
-    () => ({ lat: 38.77868168090565, lng: -9.096689988635774 }),
-    []
-  );
   return (
     <Box mt={5} mb={10}>
       <Container
@@ -33,22 +24,7 @@ export const Map = () => {
           allowfullscreen=""
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"
-        ></iframe>
-        {/*!isLoaded ? (
-          <Typography variant="body1" color="textPrimary">
-            Loading...
-          </Typography>
-        ) : (
-          <div data-aos="zoom-in">
-            <GoogleMap
-              zoom={19}
-              center={center}
-              mapContainerClassName="map-container"
-            >
-              <MarkerF position={center} />
-            </GoogleMap>
-          </div>
-        )*/}
+        />
       </Container>
     </Box>
   );
